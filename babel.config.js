ec0a1fd069a09b39fe400a0617efa9bf7d292e56
justinelut <justinequartz@gmail.com>
2024-11-23 10:01:47 +0300

    added new files configs with native wind

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,10 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+  const plugins = [];
 
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
-    ],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],  // Allows resolving from the root directory
-          alias: {
-            '@api': './src/api',
-            '@assets': './src/assets',
-            '@components': './src/components',
-            '@scenes': './src/scenes',
-            '@theme': './src/theme',
-            '@utils': './src/utils',
-          },
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'], // Optional, adds file extensions
-        },
-      ],
-    ],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+    plugins,
   };
 };
--- src/lib/validation.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-// Define your Zod validation schema
-export const signInSchema = z.object({
-  email: z.string().email("Invalid email address").nonempty("Email is required"),
-  password: z.string().min(6, "Password must be at least 6 characters long").nonempty("Password is required"),
-});
-
-export type SignInForm = z.infer<typeof signInSchema>;
