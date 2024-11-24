bf92387700e2364d3607885c6340c188b3a764da
justinelut <justinequartz@gmail.com>
2024-11-24 10:44:17 +0300

    updated tailwind config to add src reference

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "@/app/**/*.{js,jsx,ts,tsx}",
-    "@/components/**/*.{js,jsx,ts,tsx}",
-    "@/screen/**/*.{js,jsx,ts,tsx}"
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/screen/**/*.{js,jsx,ts,tsx}"
   ],
   presets: [require("nativewind/preset")],
   theme: {
